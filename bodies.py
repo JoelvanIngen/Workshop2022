b0d1c80d8b8b3456a99a26ea7b0fa76a51dcb6fd
Joël van Ingen <47477202+JoelvanIngen@users.noreply.github.com>
2022-01-18 15:59:41 +0100

    limited elliptical orbit support

--- bodies.py
+++ bodies.py
@@ -1,29 +1,34 @@
 import math
 
 class Star():
-  def __init__(self,x,y, radius, mass, temperature):
-    self.x = x
-    self.y = y
-    
-    self.radius = radius
-    self.radius_sqr2 = math.pow(radius, 2)
-    self.mass = mass
-    self.temperature = temperature
-    
-    #This is just the area of a circle
-    self.intensity = math.pi * math.pow(radius, 2)
+    def __init__(self, radius, mass, temperature):
+        self.radius = radius
+        self.radius_sqr2 = math.pow(radius, 2)
+        self.mass = mass
+        self.temperature = temperature
 
-  def foo(self):
-    pass
+
+    def initialize(self, planet, limbdarkening):
+        self.x = planet.a * planet.e
+        self.y = 0  # for now
+        # determine total intensity
+        # for now, just the area of a circle
+        self.intensity = math.pi * math.pow(self.radius, 2)
+
+
 
 class Planet():
-  def __init__(self,x,y, radius, mass, orbit_radius):
-    self.x = x
-    self.y = y
-    self.radius = radius
-    self.radius_sqr2 = math.pow(radius, 2)
-    self.mass = mass
-    self.orbit_radius = orbit_radius
+    def __init__(self,a,e, radius, mass):
+        self.a = a
+        self.e = e
+        self.radius = radius
+        self.radius_sqr2 = math.pow(radius, 2)
+        self.mass = mass
 
-  def foo(self):
-    pass
+    def initialize(self, phi):
+        self.phi = phi
+
+    def updateX(self):
+        r = (self.a * (1 - self.e*self.e)) / (1 + self.e * math.cos(self.phi))
+        self.x = r * math.cos(self.phi) + self.e * self.a
+        self.y = 0  # for now
--- main.py
+++ main.py
@@ -11,156 +11,162 @@ from tqdm import tqdm
 
 import bodies
 
-# configs
+# config
+config = {
+  'Inclination': math.pi/2,  # pi/2: full transit
+  'Number-of-steps': 100,  # number of iteration steps
+  'Number-of-points': 150,  # number of integration points per iteration step
+}
+
+
+# Constants
+G = 6.674e-11
 
 
 def main():
-  simulator = Sim(None, [Star], Planet, [])
+    # introduce simulation object and set start_values
+    sim = Sim(config)
 
-  simulator.sim()
+    # set star, planet and add moons
+    sim.addStar(
+        radius = 696340,
+        mass = 1.988e30,
+        temperature = 6000
+    )
 
-  simulator.plot()
+    sim.addPlanet(
+        a = 150000000,
+        e = .2,
+        radius = 63710,
+        mass = 0
+    )
+
+
+
+    # run simulation
+    sim.run()
+
+
+    # plot results
+    sim.plotResults()
 
 
 class Sim():
-  def __init__(self, Config, Stars, Planet, Moons):
-    self.C = Config
-    self.Stars = Stars  # list of objects
-    self.Planet = Planet  # object
-    self.Moons = Moons  # list of objects
+    def __init__(self, config):
+        self.theta = config['Inclination']
+        self.Nsteps = config['Number-of-steps']
+        self.Npoints = config['Number-of-points']
 
-    self.Nsteps = 1000
-    self.Npoints = 150
-    
-    # determine Theta_start and Theta_stop
-    sum_of_radii = 0
-    for star in self.Stars:
-      sum_of_radii += star.radius  # add stars
-    sum_of_radii += self.Planet.radius  # add planet
-    for moon in self.Moons:  # add moons
-      sum_of_radii += moon.radius
-    
-    
-    self.Theta_start = 2*math.pi - math.acos(-sum_of_radii/self.Planet.orbit_radius)
-    self.Theta_stop = 2*math.pi - math.acos(sum_of_radii/self.Planet.orbit_radius)
-
-  def sim(self):
-    Stepsize = (self.Theta_stop-self.Theta_start)/self.Nsteps
-
-    self.angles = np.arange(self.Theta_start, (self.Theta_stop + Stepsize), Stepsize)  # x on plot
-    self.intensities = []   # y on plot
-    
-    # CURRENTLY SUPPORTS ONLY 1 STAR!!
-    self.surface_per_point =  math.pow(2 * self.Planet.radius, 2) / math.pow(self.Npoints, 2)
-    
-    for self.Theta in tqdm(self.angles):
-      self.step()
-
-    # print(self.intensities)
-
-  def step(self):
-    # updating the position of planet
-    self.Planet.x = self.Planet.orbit_radius * math.cos(self.Theta)
-
-    # TODO: updating positions of the moons
-    
-    # overgebleven intensiteit: totale intensiteit - intensiteit achter overlap
-    # CURRENTLY SUPPORTS ONLY 1 STAR!!
-    thisframe_intensity = self.Stars[0].intensity - self.calcOverlapEffect()
-    # print(f'{thisframe_intensity}')
-    
-    #storing intensity in list
-    self.intensities.append(thisframe_intensity)
-
-  def calcOverlapEffect(self):  # S: star, P: planet
-    overlapeffect = 0
-
-    # determine difference between x and y values, seen from star POV
-    # CURRENTLY SUPPORTS ONLY 1 STAR!!
-    dx = self.Stars[0].x - self.Planet.x
-    dy = self.Stars[0].y - self.Planet.y
-
-    # check if there is overlap caused by the planet
-    
-    # print(f'Current edge distance to star edge: {math.sqrt(math.pow(dx,2) + math.pow(dy,2)) - (self.Stars[0].radius + self.Planet.radius)}')
-    # CURRENTLY SUPPORTS ONLY 1 STAR!!
-    if math.pow(dx,2) + math.pow(dy,2) <= math.pow(self.Stars[0].radius + self.Planet.radius, 2):
-      # planet is causing overlap
-      # CURRENTLY SUPPORTS ONLY 1 STAR!!
-      x_coords = np.arange(self.Planet.x - self.Planet.radius, self.Planet.x + self.Planet.radius, (self.Planet.radius*2)/self.Npoints)
-      y_coords = np.arange(self.Planet.y - self.Planet.radius, self.Planet.y + self.Planet.radius, (self.Planet.radius*2)/self.Npoints)
-
-      # make array lists where the x-coords and y-lists are substracted by the planet x- and y-values, to determine distance to planet center. also square them
-      x_min_planet_sqr = np.power(x_coords - self.Planet.x, 2)
-      y_min_planet_sqr = np.power(y_coords - self.Planet.y, 2)
-
-      # do the same for the star values
-      x_min_star_sqr = np.power(x_coords - self.Stars[0].x, 2)
-      y_min_star_sqr = np.power(y_coords - self.Stars[0].y, 2)
-
-      for i,x in enumerate(x_coords):
-        for j,y in enumerate(y_coords):
-          # determine distance from testpoint to middle of the planet and leave it squared
-          sqr_distance_to_planet = x_min_planet_sqr[i] + y_min_planet_sqr[j]
-          # determine if the point lies within the planet by comparing it against the radius of the planet squared
-          if sqr_distance_to_planet <= self.Planet.radius_sqr2:
-            # if code reaches here, point is on planet, so determine shade
-            # CURRENTLY SUPPORTS ONLY 1 STAR!!
-            sqr_distance_to_star = x_min_star_sqr[i] + y_min_star_sqr[j]
-            # CURRENTLY SUPPORTS ONLY 1 STAR!!
-            if sqr_distance_to_star <= self.Stars[0].radius_sqr2:
-              # if code reaches here, point is on on star, so determine shade
-              # print(overlapeffect)
-              overlapeffect += self.applyLimbDarkening()
+        self.moons_added = False  # will be set to true if moons present
 
 
+    def addStar(self, radius, mass, temperature):
+        self.Star = bodies.Star(radius, mass, temperature)
 
-    # NOT YET RELEVANT -- FIX WHEN IMPLEMENTING MOONS
-    # # check if there is overlap caused by the moons
-    # if math.pow(dx,2) + math.pow(dy,2) >= math.pow(S.radius + P.radius, 2):
-    #   # planet is causing overlap
-    #   surface_per_point =  math.pow(S.radius, 2) / math.pow(self.Npoints, 2)
-    #   overlapeffect = 0
-    #   for x in np.arange(P.x - P.radius, P.x + P.radius, (P.radius*2)/self.Npoints):
-    #     for y in np.arange(P.y - P.radius, P.y + P.radius, (S.radius*2)/self.Npoints)):
-    #       sqr_distance_to_planet = math.pow(x-P.x,2) + math.pow(y-P.y,2)
-    #       if sqr_distance_to_planet <= math.pow(P.radius, 2):
-    #         # if code reaches here, point is on planet
-    #         sqr_distance_to_star = math.pow(x-S.x,2) + math.pow(y-S.y,2)
-    #         if sqr_distance_to_star <= math.pow(S.radius, 2):
-    #           # if code reaches here, point is on on star
-    #           overlapeffect += applyLimbDarkening()
-    
-    return overlapeffect*self.surface_per_point
+    def addPlanet(self, a, e, radius, mass):
+        self.Planet = bodies.Planet(a, e, radius, mass)
+
+    def addMoon(orbit_radius, radius):
+        # check if moon list already exists, else make one
+        if not self.moons_added:
+            self.Moons = []
+
+        self.Moons.append(bodies.Moon(orbit_radius, radius))
 
 
-  def plot(self):
-    plt.plot(self.angles, self.intensities)
-    plt.show()
+    def run(self):
+        # determine sum of all radii to check starting coordinates
+        sum_of_radii = self.Star.radius + self.Planet.radius
+        if self.moons_added:
+            sum_of_radii += [moon.radius for moon in self.Moons]
 
 
-  def applyLimbDarkening(self):
-    return 1
+        # determine distance star from zero
+        d_star_from_zero = self.Planet.e * self.Planet.a
+
+        # determine where the planet should start its transit
+        x_start = d_star_from_zero - sum_of_radii
+        x_stop = d_star_from_zero + sum_of_radii
+        phi_start = math.pi - math.acos((x_start-d_star_from_zero)/(self.Planet.a - self.Planet.e*x_start))
+        phi_stop = math.pi - math.acos((x_stop-d_star_from_zero)/(self.Planet.a - self.Planet.e*x_stop))
+
+        # use steps of constant dPhi
+        stepsize = (phi_stop-phi_start)/self.Nsteps
+
+        # create list of phi values and the correspending intensities
+        self.angles = np.arange(phi_start, (phi_stop+stepsize), stepsize)  # x on plot
+        self.intensities = []  # y on plot
+
+        self.surface_per_point =  math.pow(2 * self.Planet.radius, 2) / math.pow(self.Npoints, 2)
+
+        # initialize start values for the bodies
+        self.Star.initialize(self.Planet, self.applyLimbDarkening)  # determine total intensity etc
+        print(self.angles[0])
+        self.Planet.initialize(phi = self.angles[0])
+
+        for self.phi in tqdm(self.angles):
+            self.doStep()
+
+
+    def doStep(self):
+        # update phi-value of planet
+        self.Planet.phi = self.phi
+
+        # determine new x-value for this phi
+        self.Planet.updateX()
+
+        # overgebleven intensiteit: totale intensiteit - intensiteit achter overlap
+        intensity_this_frame = self.Star.intensity - self.calcOverlapEffect()
+
+        #store intensity in list
+        self.intensities.append(intensity_this_frame)
+
+    def calcOverlapEffect(self):  # S: star, P: planet
+        overlapeffect = 0
+
+        # determine difference between star and planet x and y values, seen from star POV
+        dx = self.Star.x - self.Planet.x
+        dy = self.Star.y - self.Planet.y
+
+        # check if there is overlap caused by the planet
+        if math.pow(dx,2) + math.pow(dy,2) <= math.pow(self.Star.radius + self.Planet.radius, 2):
+            # planet is causing overlap
+            x_coords = np.arange(self.Planet.x - self.Planet.radius, self.Planet.x + self.Planet.radius, (self.Planet.radius*2)/self.Npoints)
+            y_coords = np.arange(self.Planet.y - self.Planet.radius, self.Planet.y + self.Planet.radius, (self.Planet.radius*2)/self.Npoints)
+
+            # make array lists where the x-coords and y-lists are substracted by the planet x- and y-values, to determine distance to planet center. also square them
+            x_min_planet_sqr = np.power(x_coords - self.Planet.x, 2)
+            y_min_planet_sqr = np.power(y_coords - self.Planet.y, 2)
+
+            # do the same for the star values
+            x_min_star_sqr = np.power(x_coords - self.Star.x, 2)
+            y_min_star_sqr = np.power(y_coords - self.Star.y, 2)
+
+            for i,x in enumerate(x_coords):
+                for j,y in enumerate(y_coords):
+                    # determine distance from testpoint to middle of the planet and leave it squared
+                    sqr_distance_to_planet = x_min_planet_sqr[i] + y_min_planet_sqr[j]
+                    # determine if the point lies within the planet by comparing it against the radius of the planet squared
+                    if sqr_distance_to_planet <= self.Planet.radius_sqr2:
+                        # if code reaches here, point is on planet, so determine shade
+                        sqr_distance_to_star = x_min_star_sqr[i] + y_min_star_sqr[j]
+                        if sqr_distance_to_star <= self.Star.radius_sqr2:
+                            # if code reaches here, point is on on star, so determine shade
+                            overlapeffect += self.applyLimbDarkening()
+
+        return overlapeffect*self.surface_per_point
+
+
+    def plotResults(self):
+        plt.plot(self.angles, self.intensities)
+        plt.show()
+
+
+    def applyLimbDarkening(self):
+        return 1
 
 
 
 if __name__ == "__main__":
-
-  Star = bodies.Star(
-    x=0,
-    y=0,
-    radius=696340,  # km
-    mass=0,
-    temperature = 6000  # K
-  )
-
-  Planet = bodies.Planet(
-    x=0,
-    y=0,
-    # radius=6371, #km (10 times the earth)
-    radius=63710,
-    mass=0,
-    orbit_radius = 150000000
-  )
 
   main()
